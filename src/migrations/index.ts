9bc912b5ebbb181c5ffc0edc037fc6ab03ec1bd8
w9be1tip0v <accounts@pyweop.com>
2025-05-27 21:48:24 +0900

    Update dependencies and add migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250522_154847_initial from './20250522_154847_initial'
+import * as migration_20250522_154847_initial from './20250522_154847_initial';
+import * as migration_20250527_122842 from './20250527_122842';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250522_154847_initial.down,
     name: '20250522_154847_initial',
   },
-]
+  {
+    up: migration_20250527_122842.up,
+    down: migration_20250527_122842.down,
+    name: '20250527_122842'
+  },
+];
