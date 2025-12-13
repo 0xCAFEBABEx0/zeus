6be0370f6bc67fc81fb0eb459eeafa5a8552b27f
TheBenchMark <iam@workstat.us>
2025-12-13 13:24:40 +0000

    fix: cast context to any for Sentry compatibility
    Resolve Type Error where unknown context is not assignable to ErrorContext.

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -12,5 +12,5 @@ export async function register() {
 
 export const onRequestError = async (err: Error, request: Request, context: unknown) => {
   const Sentry = await import('@sentry/nextjs')
-  Sentry.captureRequestError(err, request as any, context)
+  Sentry.captureRequestError(err, request as any, context as any)
 }
